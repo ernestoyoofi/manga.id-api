52cf6685e7eb068f2d9e7d246c0561d357210a82
Ernestoyoofi <94425296+ernestoyoofi@users.noreply.github.com>
2023-09-30 09:17:31 +0700

    Update main.js

--- router/main.js
+++ router/main.js
@@ -45,7 +45,7 @@ route.get("/manga/:id", async (req, res) => {
   try {
     const d = await komiku.Manga_Detail(req.params.id)
     if(req.query.api === "share-app" && d.isFoundReader) {
-      res.redirect(`https://komiku.id/manga/${req.params.id}`)
+      return res.redirect(`https://komiku.id/manga/${req.params.id}`)
     }
     return res.status(200).json(d)
   } catch(err) {
